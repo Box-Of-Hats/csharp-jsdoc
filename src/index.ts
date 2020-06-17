68f98bf09ea025b0a8a0ea38cea56b57f91a823f
Box-Of-Hats <ja1ke@hotmail.co.uk>
2020-06-17 23:34:58 +0100

    init

---
+++ src/index.ts
@@ -0,0 +1,93 @@
+const interfaceNameRegex = /public class ([a-zA-Z0-9?]+) /g;
+const propertyRegex = /public ([a-zA-Z?\<\>\[\]]+ [a-zA-Z0-9]+).*;/g;
+
+export interface JsDocProperty {
+    property: string;
+    type: string;
+}
+
+const typeMappings = {
+    string: "string",
+    int: "number",
+    decimal: "number",
+    float: "number",
+    bool: "boolean",
+    object: "*",
+};
+
+const jsdocType = (typeName: string, props: string) => {
+    return `
+/**
+ * @typedef {Object} ${typeName}${props}
+ **/
+    `;
+};
+
+const jsdocProp = (propertyName: string, propertyType: string) => {
+    const isList =
+        propertyType.includes("IEnumerable") || propertyType.includes("List");
+
+    const splitComponents = propertyType.split("<");
+    propertyType =
+        splitComponents.length > 1
+            ? splitComponents[1].replace(">", "")
+            : splitComponents[0];
+
+    let jsdocType = "any";
+    if (Object.keys(typeMappings).includes(propertyType)) {
+        jsdocType = typeMappings[propertyType];
+    }
+
+    if (isList) {
+        jsdocType = `${jsdocType}[]`;
+    }
+
+    return `\n * @property {${jsdocType}} ${propertyName}`;
+};
+
+export const convertClassToJsdocType = (
+    csharpClass: string,
+    classPrefix: string,
+    classSuffix: string
+): string => {
+    const interfaceName = `${classPrefix}${extractInterfaceName(
+        csharpClass
+    )}${classSuffix}`;
+
+    const props = extractProperties(csharpClass);
+
+    const jsdocProps = props
+        .map((property) => {
+            return jsdocProp(property.property, property.type);
+        })
+        .join("");
+
+    return jsdocType(interfaceName, jsdocProps);
+};
+
+export const extractInterfaceName = (tsInterface: string): string => {
+    interfaceNameRegex.lastIndex = 0;
+    let matches = interfaceNameRegex.exec(tsInterface);
+    if (!matches || matches.length === 0) {
+        return "";
+    }
+    return matches[matches.length - 1];
+};
+
+export const extractProperties = (csharpClass: string): JsDocProperty[] => {
+    propertyRegex.lastIndex = 0;
+
+    let matches = csharpClass.match(propertyRegex);
+    if (!matches) {
+        return [];
+    }
+
+    let jsdocProps: JsDocProperty[] = matches.map((match) => {
+        const components = match.split(" ");
+        return {
+            property: components[2].trim().replace(";", ""),
+            type: components[1].trim(),
+        };
+    });
+    return jsdocProps;
+};
---
+++ src/tests/tests.ts
@@ -0,0 +1,38 @@
+import assert from "assert";
+
+import {
+    convertClassToJsdocType,
+    extractInterfaceName,
+    extractProperties,
+} from "../index";
+
+suite("Interface conversion tests", () => {
+    test("generate type - single class", () => {
+        let input = `
+        public class Beans {
+            public string PropOne { get; set; }
+            public decimal PropTwo { get; set; }
+            public int PropThree { get; set; }
+            public List<int> PropFour { get; set; }
+            public IEnumerable<decimal> PropFive;
+            public IEnumerable<OtherClass> PropSix;
+        }
+        `.trim();
+
+        let expected = `
+/**
+ * @typedef {Object} Beans
+ * @property {string} PropOne
+ * @property {number} PropTwo
+ * @property {number} PropThree
+ * @property {number[]} PropFour
+ * @property {number[]} PropFive
+ * @property {any[]} PropSix
+ **/
+        `.trim();
+
+        let actual = convertClassToJsdocType(input, "", "").trim();
+
+        assert.strictEqual(actual, expected);
+    });
+});
